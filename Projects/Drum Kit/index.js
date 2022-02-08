d200bd548303d68decaa7c49ae6501c3e5c631a6
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-02-08 11:52:04 +0530

    Add jQuery to Drumkit

--- Projects/Drum Kit/index.js
+++ Projects/Drum Kit/index.js
@@ -1,54 +1,72 @@
-
-var buttons = document.querySelectorAll(".drum");
+var buttons = $(".drum");
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        playAudio(this.innerHTML);
-        animateButton(this.innerHTML);
-    });
+  buttons[i].click(function () {
+    playAudio(this.innerHTML);
+    animateButton(this.innerHTML);
+  });
 }
 
-document.addEventListener("keydown", function(event) {
-    playAudio(event.key);
-    animateButton(event.key);
+document.addEventListener("keydown", function (event) {
+  playAudio(event.key);
+  animateButton(event.key);
 });
 
 function playAudio(key) {
-
-    switch (key) {
-        case "w":
-            var audio = new Audio("sounds/tom-1.mp3");
-            break;
-        case "a":
-            var audio = new Audio("sounds/tom-2.mp3");
-            break;
-        case "s":
-            var audio = new Audio("sounds/tom-3.mp3");
-            break;
-        case "d":
-            var audio = new Audio("sounds/tom-4.mp3");
-            break;
-        case "j":
-            var audio = new Audio("sounds/snare.mp3");
-            break;
-        case "k":
-            var audio = new Audio("sounds/crash.mp3");
-            break;
-        case "l":
-            var audio = new Audio("sounds/kick-bass.mp3");
-            break;
-        default:
-            return;
-    }
-    audio.play();
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    default:
+      return;
+  }
+  audio.play();
 }
 
+// function animateButton(key) {
+//   var activeButton = document.querySelector("." + key);
+//   activeButton.classList.add("pressed");
+
+//   setTimeout(function () {
+//     activeButton.classList.remove("pressed");
+//   }, 150);
+// }
+
 function animateButton(key) {
+  var activeButton = $("." + key);
 
-    var activeButton = document.querySelector("." + key);
-    activeButton.classList.add("pressed");
-
-    setTimeout(function() {
-        activeButton.classList.remove("pressed");
-    }, 150);
+  activeButton.animate(
+    {
+      "box-shadow": " 0 3px 4px 0 #DBEDF3",
+      opacity: 0.5,
+    },
+    150,
+    function () {
+      activeButton.animate(
+        {
+          "box-shadow": "none",
+          opacity: 1,
+        },
+        150
+      );
+    }
+  );
 }
