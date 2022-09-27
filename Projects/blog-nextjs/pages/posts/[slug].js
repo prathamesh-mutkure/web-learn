fdb5f772cee5dd8b1d52d2e317634395d53450e7
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-09-27 19:51:05 +0530

    Next Project: Blog Website Starting

---
+++ Projects/blog-nextjs/pages/posts/[slug].js
@@ -0,0 +1,9 @@
+const PostDetailPage = () => {
+  return (
+    <div>
+      <h1>Post Detail Page</h1>
+    </div>
+  );
+};
+
+export default PostDetailPage;
---
+++ Projects/blog-nextjs/pages/posts/index.js
@@ -0,0 +1,9 @@
+const AllPostPage = () => {
+  return (
+    <div>
+      <h1>All Post Page</h1>
+    </div>
+  );
+};
+
+export default AllPostPage;
